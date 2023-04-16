d975c86edd4efafd6490c08acc5be0345d9702d7
hasankarim18 <hallysust@gmail.com>
2023-04-16 18:13:35 +0600

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+
+
+### my firebase id: hasan.course.2087
+- When ever any body want't to use it they have to use their firebase.config.js file and own firebase setup
+- From the firebase.config.js file you have to export `app` and `auth`
+const
---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <div className="App">
+      <div className="container mx-auto">
+        <h1 className="text-3xl">Auth</h1>
+      </div>
+    </div>
+  )
+}
+
+export default App
